292392ccfbd5bea780d3491060bbf4800740a82f
Mariia Gazizova <mgazizova5@gmail.com>
2022-07-17 18:09:25 +0200

    Initial Commit

---
+++ FindFGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindFGame
+//
+//  Created by Мария Газизова on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
